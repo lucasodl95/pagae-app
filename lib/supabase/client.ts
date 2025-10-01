4788b353c5e477cb422ffab7e8256cec55a1648a
lucas <lacerdaodl2@gmail.com>
2025-10-01 20:36:54 -0300

    2

--- lib/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/types/database'
-
-export const createClient = () => {
-  return createClientComponentClient<Database>()
-}
-
-export const supabase = createClient()
